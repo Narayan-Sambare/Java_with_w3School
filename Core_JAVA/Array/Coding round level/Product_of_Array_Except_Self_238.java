2c541fc40bdb462eaf06d079e0013720cc87f1be
Sanket <sambarenarayan@gmail.com>
2026-04-27 14:51:25 +0530

    implement leet code code  level medium problem number 238 product of array except self

---
+++ Core_JAVA/Array/Coding round level/Product_of_Array_Except_Self_238.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Product_of_Array_Except_Self_238 {
+    public static void main(String[] args) {
+        int res[]=productExceptSelf(new int[]{1,2,3,4});
+
+        Arrays.stream(res).forEach((int i)-> System.out.print(i+" "));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        result[0] = 1; 
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * suffix;
+            suffix *= nums[i];
+        }
+        return result;
+    }
+}
