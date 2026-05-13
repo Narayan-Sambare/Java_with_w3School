be74ec55350571d09512cbd07896b562c4bcca2b
Sanket <sambarenarayan@gmail.com>
2026-05-13 10:51:31 +0530

    LongestSubarray_1493

--- Core_JAVA/Array/Coding round level/LongestSubarray_1493.java
+++ Core_JAVA/Array/Coding round level/LongestSubarray_1493.java
@@ -3,15 +3,10 @@ class LongestSubarray_1493 {
         int left = 0;
         int zeros = 0;
         int right;
-
         for (right = 0; right < nums.length; right++) {
-            // If we hit a 0, increment our zero counter
             if (nums[right] == 0) {
                 zeros++;
             }
-
-            // If we have more than one zero, "shift" the window 
-            // by moving the left pointer
             if (zeros > 1) {
                 if (nums[left] == 0) {
                     zeros--;
@@ -19,9 +14,6 @@ class LongestSubarray_1493 {
                 left++;
             }
         }
-
-        // The result is the window size minus 1 
-        // (because we MUST delete one element)
         return right - left - 1;
     }
 }
