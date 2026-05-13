5fd8025e873555db1cb814417a941c3be3e4f817
Sanket <sambarenarayan@gmail.com>
2026-05-13 10:52:18 +0530

    FileReaderDemo

---
+++ Core_JAVA/File_Handling/FileReaderDemo.java
@@ -0,0 +1,15 @@
+package File_handling;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("test.txt");
+		int i;
+		while ((i = fr.read()) != -1) {
+		    System.out.print((char)i);
+		}
+		fr.close();
+	}
+}
